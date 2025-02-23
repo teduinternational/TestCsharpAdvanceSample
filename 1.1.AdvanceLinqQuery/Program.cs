821586862d2597260e655c4c8c76d923800c128d
Toan bach <ngoctoan@live.com>
2025-02-23 07:27:37 +0700

    Multi-threading

--- 1.1.AdvanceLinqQuery/Program.cs
+++ 1.1.AdvanceLinqQuery/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿FileReader resource = null;
+try
+{
+    resource = new FileReader("file.txt");
+    resource.ReadFile();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
+finally
+{
+    if (resource != null)
+    {
+        resource.Dispose();
+    }
+}
+// Dispose sẽ được gọi tự động ở đây, kể cả khi xảy ra lỗi
+
+public class FileReader : IDisposable
+{
+    private FileStream _fileStream;
+
+    public FileReader(string filePath)
+    {
+        _fileStream = new FileStream(filePath, FileMode.Open);
+    }
+
+    public void ReadFile()
+    {
+        // Logic đọc tệp
+    }
+
+    public void Dispose()
+    {
+        // Giải phóng tài nguyên khi không còn sử dụng
+        _fileStream?.Dispose();
+    }
+}
---
+++ 4.1.Multithreading/Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    static int counter = 0;
+    static object lockObject = new object();
+    private static async Task Main(string[] args)
+    {
+        Thread thread = new Thread(DoWork);
+        thread.Start();
+
+        Console.WriteLine("Main thread is running...");
+
+        thread.Join();
+
+        Console.WriteLine("Main thread is done.");
+
+        // Tạo và chạy một task đồng thời
+        Task task = Task.Run(() => DoWork());
+
+        // Công việc trong luồng chính
+        Console.WriteLine("Main task is running...");
+
+        // Chờ task hoàn thành
+        await task;
+
+        Console.WriteLine("Main task finished.");
+
+
+        Task task1 = Task.Run(() => DoWorkWithId(1));
+        Task task2 = Task.Run(() => DoWorkWithId(2));
+
+        await Task.WhenAll(task1, task2);
+
+        Console.WriteLine("Both tasks completed.");
+
+        Thread t1 = new Thread(IncrementCounter);
+        Thread t2 = new Thread(IncrementCounter);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine($"Final counter value: {counter}");
+
+
+        static void DoWork()
+        {
+            Console.WriteLine("Worker thread is running...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Worker thread is done.");
+        }
+
+        static void DoWorkWithId(int id)
+        {
+            Console.WriteLine($"Worker thread {id} is running...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Worker thread {id} is done.");
+        }
+
+        static void IncrementCounter()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                lock (lockObject)
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+}
