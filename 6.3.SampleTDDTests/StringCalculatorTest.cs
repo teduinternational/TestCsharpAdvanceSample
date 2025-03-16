c5f85f684064051cccc7d9bb75cd3f38cee32df7
Toan bach <ngoctoan@live.com>
2025-03-16 20:00:43 +0700

    TDD

---
+++ 6.3.SampleTDDTests/StringCalculatorTest.cs
@@ -0,0 +1,65 @@
+﻿using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3.SampleTDDTests
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            // Arrange
+            var calculator = new StringCalculator();
+            // Act
+            var result = calculator.Add("");
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Add_SingleNumber_ReturnsTheNumber()
+        {
+            // Arrange
+            var calculator = new StringCalculator();
+            // Act
+            var result = calculator.Add("5");
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Add_TwoNumbers_ReturnsTheSum()
+        {
+            // Arrange
+            var calculator = new StringCalculator();
+            // Act
+            var result = calculator.Add("5,3");
+            // Assert
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void Add_MultipleNumbers_ReturnsTheSum()
+        {
+            // Arrange
+            var calculator = new StringCalculator();
+            // Act
+            var result = calculator.Add("5,3,8,1");
+            // Assert
+            Assert.Equal(17, result);
+        }
+
+        [Fact]
+        public void Add_StringWithNonNumericCharacters_ThrowsException()
+        {
+            // Arrange
+            var calculator = new StringCalculator();
+            // Assert
+            Assert.Equal(17, calculator.Add("5,3,8,1,x"));
+        }
+    }
+}
---
+++ Calculator/StringCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            return numbers.Split(',')
+                .Where(n => int.TryParse(n, out _))
+                .Select(int.Parse)
+                .Sum();
+        }
+    }
+}
